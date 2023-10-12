960ea9663170ba21cc62511eb0d96e7926899f4f
rsennaco <rsennaco@gmail.com>
2023-10-12 11:19:57 -0400

    apply secrets bash

---
+++ Monitoring/Bash/apply-secrets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script applies secrets to kubernetes
+
+kubectl apply -f #file1
+kubectl apply -f #file1
+kubectl apply -f #file1
+kubectl apply -f #file1
+kubectl apply -f #file1
